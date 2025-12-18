a12232913c9d4b42e7b2580fedc5dc08c6c7d6d6
Matis Toniutti <matistoniutti@hotmail.fr>
2025-12-18 14:33:58 +0100

    Add filtered offers by title

--- linkedinScrapping.py
+++ linkedinScrapping.py
@@ -2,7 +2,13 @@ from playwright.sync_api import sync_playwright
 import time
 import random
 
+
 def run_scraper():
+    #liste des mots qu'on ne veut pas dans une offre
+    banned_words = ["confirmé","product owner","stage","internship","alternance","stagiaire","intern","alternant","interim","freelance","docteur","phd","senior","expert","consultant","annotator","data engineer"]
+    #liste des mots qui permettent de considérer une offre
+    needed_words = ["ia", "ai","data", "ml", "cv", "nlp", "llm", "agent"]
+
     # Le "with" garantit que le navigateur se ferme proprement même s'il y a une erreur (pas de processus fantomes)
     with sync_playwright() as p:
         # headless=False pour voir chromium en live et les actions de playwright
@@ -22,10 +28,8 @@ def run_scraper():
         # on récupère la barre de scroll permettant de scroller les offres sinon on ne peut pas toutes les récupérer
         conteneur_scroll = offres_locator.first.locator('xpath=./../../../..')
 
-        for i in range(10):
-            # descendre la div de 500 pixels
-            conteneur_scroll.evaluate("el => el.scrollTop += 500")
-            time.sleep(1)
+        scroll_element(conteneur_scroll,distance=2500)
+        
 
         
         print(f"Nombre d'offres trouvées : {offres_locator.count()}")
@@ -34,7 +38,20 @@ def run_scraper():
 
             job_id = offre.get_attribute("data-job-id")
             
-            print(f"Offre n°{index} [ID:{job_id}] ")
+            title_locator = offre.locator("strong").first
+    
+            title = title_locator.inner_text().strip()
+
+            is_banned = any(word in title.lower() for word in banned_words)
+            is_needed = any(word in title.lower() for word in needed_words)
+
+            if not is_banned and is_needed:
+                print(f"Acceptée : ID: {job_id} | Titre: {title}")
+                offre.click()
+                time.sleep(1)
+            else:
+                print(f"Refusée : ID: {job_id} | Titre: {title}")
+
 
 
         page.pause()
@@ -71,6 +88,19 @@ def scroll(page, direction="down", sleep_min = 1, sleep_max = 2):
             break
         
         time.sleep(random.uniform(sleep_min, sleep_max))
+
+def scroll_element(element, distance=1000):
+    pixels_parcourus = 0
+    while pixels_parcourus < distance:
+        # On fait des sauts de 40 pixels (unité de coup de molette)
+        step = 40
+        # chaque accoup avec le doigt
+        for i in range(random.randint(4,7)):
+            element.evaluate(f"el => el.scrollTop += {step}")
+            pixels_parcourus += step
+            
+            time.sleep(random.uniform(0.02, 0.06))
+        time.sleep(random.uniform(0.1,0.3))
 
 if __name__ == "__main__":
     run_scraper()
