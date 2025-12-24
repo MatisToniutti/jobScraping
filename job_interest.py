93691280c418d0c802d37f8473878dd71c630223
Matis Toniutti <matistoniutti@hotmail.fr>
2025-12-24 16:23:59 +0100

    Change model to Qwen3-8B

--- job_interest.py
+++ job_interest.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, BitsAndBytesConfig, Gemma3ForCausalLM
+from transformers import AutoTokenizer, BitsAndBytesConfig, AutoModelForCausalLM
 import torch
 from sqlitedb import get_connection, get_unprocessed_offers, set_interest_offer
 
@@ -7,12 +7,15 @@ def give_interest_to_jobs():
     conn = get_connection()
     offers = get_unprocessed_offers(conn)
 
-    model_id = "google/gemma-3-1b-it"
+    model_id = "Qwen/Qwen3-4B-Instruct-2507"
 
     quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 
-    model = Gemma3ForCausalLM.from_pretrained(
-        model_id, quantization_config=quantization_config
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype="auto",
+        device_map="auto",
+        quantization_config=quantization_config
     ).eval()
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -21,24 +24,22 @@ def give_interest_to_jobs():
         messages = [
             {
                 "role": "system",
-                "content": [{"type": "text", "text": f"Tu es un filtreur d'offres d'emploi strict. Ta mission est de rejeter (NO) toute offre qui ne respecte pas parfaitement mes limites. REJETTE AUTOMATIQUEMENT (NO) SI : L'offre demande PLUS de 2 ans d'expérience ; Le poste est orienté Gouvernance, Management ou Data Officer (je veux de la TECHNIQUE/FINE-TUNING) ; Ce n'est pas du Python. Réponds par YES uniquement si TOUTES les conditions sont réunies. Sinon, réponds NO. Réponds sous ce format : Raison du rejet : (écris la raison ici) Verdict : (YES ou NO)"}]
+                "content": "Tu es un filtreur d'offres d'emploi. Moi je suis une personne en recherche d'emploi ayant fini son bac+5 en informatique avec spécialité IA, j'ai 1 an d'expérience en stage, et je souhaite trouver un CDI dans le domaine de l'IA. Ta mission est de rejeter toute offre qui ne respecte pas mes limites. REJETTE SI : L'offre demande explicitement PLUS de 2 ans d'expérience; L'offre mentionne explicitement que le rôle à pourvoir est un Gouvernant, un Manager ou un Consultant; L'offre mentionne explicitement que le type de contrat n'est pas un CDI. Réponds par YES uniquement si aucune contrainte n'est franchie ou si tu n'es pas sûr, je préfère garder trop d'offres que l'inverse. Sinon, réponds NO. Ensuite explique pourquoi tu as fait ce choix"
             },
             {
                 "role": "user",
-                "content": [{"type": "text", "text": f"Voici la description de l'offre : \n\n {offer["description"]}"}]
+                "content": f"Voici la description de l'offre : \n\n {offer["description"]}"
             }
         ]
-        inputs = tokenizer.apply_chat_template(
+        text = tokenizer.apply_chat_template(
             messages,
             add_generation_prompt=True,
-            tokenize=True,
-            return_dict=True,
-            return_tensors="pt",
-        ).to(model.device)#.to(torch.bfloat16)
-
+            tokenize=False,
+        )
+        inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
         with torch.inference_mode():
-            outputs = model.generate(**inputs, max_new_tokens=64)
+            outputs = model.generate(**inputs, max_new_tokens=512)
 
         input_length = inputs.input_ids.shape[1]
         new_tokens = outputs[0][input_length:]
@@ -47,7 +48,6 @@ def give_interest_to_jobs():
         print(f"id : {offer["id"]}")
         print(f"Résultat : {response}")
         lines = response.strip().split('\n')
-        verdict_line = lines[-1].lower() # On prend la dernière ligne
 
         # if response == "no":
         #     set_interest_offer(conn, 2, offer["id"])
