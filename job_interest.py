ff9c4171e2098b9a755c5cf62081605b51107f07
Matis Toniutti <matistoniutti@hotmail.fr>
2025-12-24 16:43:05 +0100

    speedup

--- job_interest.py
+++ job_interest.py
@@ -9,11 +9,16 @@ def give_interest_to_jobs():
 
     model_id = "Qwen/Qwen3-4B-Instruct-2507"
 
-    quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",           # Format optimisé pour la précision
+        bnb_4bit_use_double_quant=True,       
+        bnb_4bit_compute_dtype=torch.bfloat16 
+    )
 
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
-        torch_dtype="auto",
+        torch_dtype=torch.bfloat16,
         device_map="auto",
         quantization_config=quantization_config
     ).eval()
@@ -24,7 +29,7 @@ def give_interest_to_jobs():
         messages = [
             {
                 "role": "system",
-                "content": "Tu es un filtreur d'offres d'emploi. Moi je suis une personne en recherche d'emploi ayant fini son bac+5 en informatique avec spécialité IA, j'ai 1 an d'expérience en stage, et je souhaite trouver un CDI dans le domaine de l'IA. Ta mission est de rejeter toute offre qui ne respecte pas mes limites. REJETTE SI : L'offre demande explicitement PLUS de 2 ans d'expérience; L'offre mentionne explicitement que le rôle à pourvoir est un Gouvernant, un Manager ou un Consultant; L'offre mentionne explicitement que le type de contrat n'est pas un CDI. Réponds par YES uniquement si aucune contrainte n'est franchie ou si tu n'es pas sûr, je préfère garder trop d'offres que l'inverse. Sinon, réponds NO. Ensuite explique pourquoi tu as fait ce choix"
+                "content": "Tu es un filtreur d'offres d'emploi. Moi je suis une personne en recherche d'emploi ayant fini son bac+5 en informatique avec spécialité IA, j'ai 1 an d'expérience en stage, et je souhaite trouver un CDI dans le domaine de l'IA. Ta mission est de rejeter toute offre qui ne respecte pas mes limites. REJETTE SI : L'offre demande explicitement PLUS de 2 ans d'expérience; L'offre mentionne explicitement que le rôle à pourvoir est un Gouvernant, un Manager ou un Consultant; Réponds par YES uniquement si aucune contrainte n'est franchie ou si tu n'es pas sûr, je préfère garder trop d'offres que l'inverse. Sinon, réponds NO. Ensuite explique rapidement pourquoi tu as fait ce choix"
             },
             {
                 "role": "user",
