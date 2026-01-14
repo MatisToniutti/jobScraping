ef1efb8e281208f482c02e8cd8e6d081fb89cf8b
Matis Toniutti <matistoniutti@hotmail.fr>
2026-01-14 12:24:55 +0100

    more robust linkedin scrapping

--- linkedin_scrapping.py
+++ linkedin_scrapping.py
@@ -36,87 +36,100 @@ def run_scraper():
         page = context.new_page()
         
         for offers_link in offers_links:
-            print(offers_link)
-            page.goto(offers_link)
+            try:
+                print(f"Ouverture de : {offers_link}")
+                page.goto(offers_link)
 
-            next_page = True
+                next_page = True
 
-            while next_page:
-                #on attend qu'au moins une offre ait chargée
-                page.wait_for_selector('[data-job-id]')
-                
-                # on récupère un "locator" qui pointe vers toutes les div ayant l'attribut data-job-id (les offres)
-                offres_locator = page.locator('div[data-job-id]')
-
-                # on récupère la barre de scroll permettant de scroller les offres sinon on ne peut pas toutes les récupérer
-                conteneur_scroll = offres_locator.first.locator('xpath=./../../../..')
-                scroll_element(conteneur_scroll,distance=3000)
-                
-                # print(f"Nombre d'offres trouvées : {offres_locator.count()}")
-                
-                for offre in offres_locator.all():
-
+                while next_page:
                     try:
+                        #on attend qu'au moins une offre ait chargée
+                        page.wait_for_selector('[data-job-id]', timeout=10000)
+                    except:
+                        print("Aucune offre trouvée sur cette page ou chargement trop lent.")
+                        break
+                    
+                    # on récupère un "locator" qui pointe vers toutes les div ayant l'attribut data-job-id (les offres)
+                    offres_locator = page.locator('div[data-job-id]')
 
-                        job_id = offre.get_attribute("data-job-id")
-                        
-                        title_locator = offre.locator("strong").first
-                        title = title_locator.inner_text().strip()
+                    # on récupère la barre de scroll permettant de scroller les offres sinon on ne peut pas toutes les récupérer
+                    conteneur_scroll = offres_locator.first.locator('xpath=./../../../..')
+                    scroll_element(conteneur_scroll,distance=3000)
+                    
+                    # print(f"Nombre d'offres trouvées : {offres_locator.count()}")
+                    
+                    for offre in offres_locator.all():
 
-                        is_banned = any(word in title.lower() for word in banned_words)
-                        is_needed = any(word in title.lower() for word in needed_words)
+                        try:
 
-                        if not is_banned and is_needed:
-                            link = f"https://www.linkedin.com/jobs/view/{job_id}/"
-                            # print(f"Acceptée : ID: {job_id} | Titre: {title}")
-                            offre.click()
-                            offer_details = page.locator(".jobs-search__job-details")
-                            offer_details.wait_for(state="visible", timeout=5000)
-                            company_selector = ".job-details-jobs-unified-top-card__company-name a"
-                            company_locator = offer_details.locator(company_selector).first
-                            # count() est instantané, il ne force pas d'attente
-                            if company_locator.count() > 0:
-                                company_name = company_locator.inner_text().strip()
-                            else:
-                                company_name = "Inconnu"
-                            location_selector = ".job-details-jobs-unified-top-card__primary-description-container span span"
-                            location = offer_details.locator(location_selector).first.inner_text().strip()
-                            location = location.split(',')
-                            location = [s.strip() for s in location]
-                            description_selector = ".jobs-description__container div div div p"
-                            description = offer_details.locator(description_selector).first.inner_text().strip()
-                            # print(f"Entreprise trouvée : {company_name}")
-                            # print(f"Emplacement : {location}")
-                            # print(f"Description : {description}")
-                            # page.pause()
+                            title_locator = offre.locator("strong").first
+                            if title_locator.count() == 0: continue
+                            title = title_locator.inner_text(timeout=2000).strip()
+
+                            is_banned = any(word in title.lower() for word in banned_words)
+                            is_needed = any(word in title.lower() for word in needed_words)
+
+                            if not is_banned and is_needed:
+                                job_id = offre.get_attribute("data-job-id")
+                                link = f"https://www.linkedin.com/jobs/view/{job_id}/"
+                                # print(f"Acceptée : ID: {job_id} | Titre: {title}")
+                                offre.click()
+                                time.sleep(0.1)
+
+                                offer_details = page.locator(".jobs-search__job-details")
+                                offer_details.wait_for(state="visible", timeout=5000)
+
+                                company_selector = ".job-details-jobs-unified-top-card__company-name a"
+                                company_locator = offer_details.locator(company_selector).first
+                                # count() est instantané, il ne force pas d'attente
+                                if company_locator.count() > 0:
+                                    company_name = company_locator.inner_text().strip()
+                                else:
+                                    company_name = "Inconnu"
+
+                                loc_locator = offer_details.locator(".job-details-jobs-unified-top-card__primary-description-container span span").first
+                                location_raw = loc_locator.inner_text(timeout=2000).strip() if loc_locator.count() > 0 else ""
+                                location = [s.strip() for s in location_raw.split(',')]
+
+                                desc_locator = offer_details.locator(".jobs-description__container div div div p").first
+                                if desc_locator.count() > 0:
+                                    description = desc_locator.inner_text(timeout=2000).strip()
+                                else:
+                                    continue
+                                print("insert")
+                                insert_offer(conn,
+                                            job_id="linkedin-"+job_id,
+                                            website="linkedin",
+                                            company=company_name,
+                                            description=description,
+                                            city=location[0] if len(location) >2 else "",
+                                            state=location[1] if len(location) >2 else "",
+                                            country=location[2] if len(location) >2 else "",
+                                            name=title,
+                                            link=link)
+                            # else:
+                            #     print(f"Refusée : ID: {job_id} | Titre: {title}")
+
+                        except TimeoutError:
+                            print(f"Erreur : L'offre a mis trop de temps à charger. Passage à la suivante.")
+                            continue
+                        except Exception as e:
+                            print(f"Une erreur imprévue est survenue : {e}")
+                            continue
                             
-                            insert_offer(conn,
-                                        job_id="linkedin-"+job_id,
-                                        website="linkedin",
-                                        company=company_name,
-                                        description=description,
-                                        city=location[0] if len(location) >2 else "",
-                                        state=location[1] if len(location) >2 else "",
-                                        country=location[2] if len(location) >2 else "",
-                                        name=title,
-                                        link=link)
-                        # else:
-                        #     print(f"Refusée : ID: {job_id} | Titre: {title}")
 
-                    except TimeoutError:
-                        print(f"Erreur : L'offre a mis trop de temps à charger. Passage à la suivante.")
-                        continue
-                    except Exception as e:
-                        print(f"Une erreur imprévue est survenue : {e}")
-                        continue
-                        
+                    next_button = page.locator('button[aria-label="View next page"]')
+                    
+                    if next_button.count()>0:
+                        next_button.click()
+                    else:
+                        next_page=False
 
-                next_button = page.locator('button[aria-label="View next page"]')
-                
-                if next_button.count()>0:
-                    next_button.click()
-                else:
-                    next_page=False
+            except Exception as e:
+                # Ici on attrape les erreurs au niveau de la PAGE/LIEN
+                print(f"Erreur critique sur le lien {offers_link} : {e}")
+                continue
 
         browser.close()
 
@@ -157,12 +170,12 @@ def scroll_element(element, distance=1000):
         # On fait des sauts de 40 pixels (unité de coup de molette)
         step = 40
         # chaque accoup avec le doigt
-        for i in range(random.randint(4,7)):
+        for i in range(random.randint(7,10)):
             element.evaluate(f"el => el.scrollTop += {step}")
             pixels_parcourus += step
             
             time.sleep(random.uniform(0.02, 0.04))
-        time.sleep(random.uniform(0.1,0.2))
+        time.sleep(random.uniform(0.05,0.15))
 
 if __name__ == "__main__":
     run_scraper()
