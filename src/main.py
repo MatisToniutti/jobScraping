afc5b2325ff0398f3a2e6ace923357cbda322c71
Matis Toniutti <matistoniutti@hotmail.fr>
2026-01-19 16:31:23 +0100

    add wttj scraping to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import scrapers.linkedin_scraping as linkedin
 import scrapers.apec_scraping as apec
 import scrapers.france_travail_scraping as ft
+import scrapers.WTTJ_scraping as wttj
 from models.job_interest import give_interest_to_jobs
 from utils.utils import export_links
 
@@ -9,6 +10,8 @@ def main():
     print("Apec scraping effectué")
     ft.run_scraper()
     print("france travail scraping effectué")
+    wttj.run_scraper()
+    print("welcome to the jungle scraping effectué")
     linkedin.run_scraper()
     print("linkedin scraping effectué")
     
